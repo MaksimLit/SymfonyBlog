0da9f6effca3b20fb80a1ac04e2b5295f988f513
Maksim Litvyakov <litvy-maksim@yandex.ru>
2020-09-09 15:46:57 +0700

    Changed PsotsController
    * Output fake data to a template posts/index.html.twig

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -2,28 +2,40 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostsController extends AbstractController
 {
-    /**
-     * @Route("/posts", name="posts")
-     */
-    public function index()
+    /** @var PostRepository $postRepository */
+    private $postRepository;
+
+    public function __construct(PostRepository $postRepository)
     {
-        $posts = [
-            'post_1' => [
-                'title' => 'Заголовок первого поста',
-                'body' => 'Тело первого поста'
-            ],
-            'post_2' => [
-                'title' => 'Заголовок второго поста',
-                'body' => 'Тело второго поста'
-            ]
-        ];
+        $this->postRepository = $postRepository;
+    }
+
+    /**
+     * @Route("/posts", name="blog_posts")
+     */
+    public function posts()
+    {
+        $posts = $this->postRepository->findAll();
+
         return $this->render('posts/index.html.twig', [
-            'posts' => $posts,
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * @Route("/posts/{slug}", name="blog_show")
+     */
+    public function post(Post $post)
+    {
+        return $this->render('posts/show.html.twig', [
+            'post' => $post
         ]);
     }
 }
