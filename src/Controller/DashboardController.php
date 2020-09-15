d260e5fb7d01007c6d0c68a2d83461decc4d68a1
Maksim Litvyakov <litvy-maksim@yandex.ru>
2020-09-15 16:13:07 +0700

    Added admin functions
    * Added the functions of adding new users (EasyAdmin)
    * Added restriction on entering the admin panel

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Controller\Admin\PostCrudController;
+use App\Entity\Post;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -20,5 +22,12 @@ class DashboardController extends AbstractDashboardController
         $routeBuilder = $this->get(CrudUrlGenerator::class)->build();
 
         return $this->redirect($routeBuilder->setController(PostCrudController::class)->generateUrl());
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::section('Impotent');
+        yield MenuItem::linkToCrud('Posts', 'fa fa-file-pdf', Post::class);
+        yield MenuItem::linkToCrud('Users', 'fa fa-file-pdf', User::class);
     }
 }
